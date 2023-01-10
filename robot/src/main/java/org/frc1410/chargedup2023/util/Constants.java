5c73526a4765cd7fb746c2378d66271d7363e7f3
Rowan <717538@dpsk12.net>
2023-01-09 22:33:31 -0700

    fix(Auto): Changed constants to (Hopefully) 2020 versions

--- robot/src/main/java/org/frc1410/chargedup2023/util/Constants.java
+++ robot/src/main/java/org/frc1410/chargedup2023/util/Constants.java
@@ -7,14 +7,12 @@ public interface Constants {
     int OPERATOR_CONTROLLER = 1;
 
     // DRIVETRAIN
-    double KS = 0;
-    double KV = 0;
-    double KA = 0;
-    double KV_ANGULAR = 0;
-    double KA_ANGULAR = 0;
+    double KS = 0.676;
+    double KV = 2.67;
+    double KA = 0.34;
     double GEARING = (11.0 / 62) * (24.0 / 54) * 1.057;
     double METERS_PER_REVOLUTION = .478778;
     double ENCODER_CONSTANT = (1 / GEARING) * (1. / 2048.) * METERS_PER_REVOLUTION;
-    double TRACKWIDTH = 0.6907; //Meters
+    double TRACKWIDTH = 0.615; //Meters
     DifferentialDriveKinematics KINEMATICS = new DifferentialDriveKinematics(TRACKWIDTH);
 }
