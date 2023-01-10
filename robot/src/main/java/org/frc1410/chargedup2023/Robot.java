2add23f48945a4d74106c186cd04cc049386a4b4
Rowan Eklund <717538@dpsk12.net>
2023-01-10 08:51:27 -0700

    fix: Readded drivetrain flipping

--- robot/src/main/java/org/frc1410/chargedup2023/Robot.java
+++ robot/src/main/java/org/frc1410/chargedup2023/Robot.java
@@ -1,6 +1,7 @@
 package org.frc1410.chargedup2023;
 
 import org.frc1410.chargedup2023.commands.TankDriveLooped;
+import org.frc1410.chargedup2023.commands.FlipDrivetrainAction;
 import org.frc1410.chargedup2023.commands.groups.auto.Test1MeterAuto;
 import org.frc1410.chargedup2023.commands.groups.auto.Test2MeterAuto;
 import org.frc1410.chargedup2023.commands.groups.auto.Test90DegreeAuto;
@@ -8,6 +9,7 @@ import org.frc1410.chargedup2023.subsystem.Drivetrain;
 import org.frc1410.framework.AutoSelector;
 import org.frc1410.framework.PhaseDrivenRobot;
 import org.frc1410.framework.control2.Controller;
+import org.frc1410.framework.scheduler.task.CommandTask;
 import org.frc1410.framework.scheduler.task.TaskPersistence;
 
 import static org.frc1410.chargedup2023.util.Constants.*;
@@ -27,5 +29,6 @@ public final class Robot extends PhaseDrivenRobot {
     @Override
     public void teleopSequence() {
         scheduler.scheduleDefaultCommand(new TankDriveLooped(drivetrain, driverController.LEFT_Y_AXIS, driverController.RIGHT_Y_AXIS), TaskPersistence.GAMEPLAY);
+        driverController.LEFT_BUMPER.scheduler().schedule(new CommandTask(new FlipDrivetrainAction(drivetrain, driverController)), TaskPersistence.EPHEMERAL);
     }
 }
