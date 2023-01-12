4ead18faf53eefd8a83057dd222243968ced340d
Rowan Eklund <717538@dpsk12.net>
2023-01-11 17:13:54 -0700

    feat(Auto): added reset position

--- robot/src/main/java/org/frc1410/chargedup2023/commands/groups/auto/TestQuarterCircleAuto.java
+++ robot/src/main/java/org/frc1410/chargedup2023/commands/groups/auto/TestQuarterCircleAuto.java
@@ -5,8 +5,13 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import org.frc1410.chargedup2023.subsystem.Drivetrain;
 import org.frc1410.chargedup2023.util.Trajectories;
 
+import static org.frc1410.chargedup2023.auto.POIs.START;
+
 public class TestQuarterCircleAuto extends SequentialCommandGroup {
 	public TestQuarterCircleAuto(Drivetrain drivetrain) {
+		drivetrain.resetPoseEstimation(START);
+		System.out.println("Running Quarter Circle");
+
 		addCommands(
 				Trajectories.testQuarterCircle(drivetrain),
 				new RunCommand(() -> drivetrain.tankDriveVolts(0, 0))
