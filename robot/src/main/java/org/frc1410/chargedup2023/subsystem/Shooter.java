1d6a8ebd6e83df88d1371a0370184af9ae7ae5d4
Cameron Delong <cameron.delong@icloud.com>
2023-01-12 18:45:12 -0700

    feat: Set shooter and vertical storage motor ids

--- robot/src/main/java/org/frc1410/chargedup2023/subsystem/Shooter.java
+++ robot/src/main/java/org/frc1410/chargedup2023/subsystem/Shooter.java
@@ -5,7 +5,7 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public class Shooter implements Subsystem {
-    private final WPI_TalonFX motor = new WPI_TalonFX(0);
+    private final WPI_TalonFX motor = new WPI_TalonFX(30);
 
     public Shooter() {
         // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
