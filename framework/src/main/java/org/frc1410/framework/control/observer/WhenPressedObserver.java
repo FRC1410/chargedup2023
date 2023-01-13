6a7e3095195adee663983153dd4880beae75a471
Rowan Eklund <717538@dpsk12.net>
2023-01-12 17:41:20 -0700

    fix(Framework): Updated with main2

---
+++ framework/src/main/java/org/frc1410/framework/control/observer/WhenPressedObserver.java
@@ -0,0 +1,22 @@
+package org.frc1410.framework.control.observer;
+
+import org.frc1410.framework.control.Button;
+import org.frc1410.framework.scheduler.task.LifecycleHandler;
+import org.frc1410.framework.scheduler.task.observer.Observer;
+import org.jetbrains.annotations.NotNull;
+
+public class WhenPressedObserver implements Observer {
+
+    private final Button button;
+
+    public WhenPressedObserver(Button button) {
+        this.button = button;
+    }
+
+    @Override
+    public void tick(@NotNull LifecycleHandler lifecycle) {
+        if (button.isActive()) {
+            lifecycle.requestExecution();
+        }
+    }
+}
---
+++ framework/src/main/java/org/frc1410/framework/control/observer/WhileHeldObserver.java
@@ -0,0 +1,24 @@
+package org.frc1410.framework.control.observer;
+
+import org.frc1410.framework.control.Button;
+import org.frc1410.framework.scheduler.task.LifecycleHandler;
+import org.frc1410.framework.scheduler.task.observer.Observer;
+import org.jetbrains.annotations.NotNull;
+
+public class WhileHeldObserver implements Observer {
+
+    private final Button button;
+
+    public WhileHeldObserver(Button button) {
+        this.button = button;
+    }
+
+    @Override
+    public void tick(@NotNull LifecycleHandler lifecycle) {
+        if (button.isActive()) {
+            lifecycle.requestExecution();
+        } else {
+            lifecycle.requestInterruption();
+        }
+    }
+}
