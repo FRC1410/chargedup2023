53e028e01489adff0f9dc1c5d49e8bb90bf30ea3
Cameron Delong <cameron.delong@icloud.com>
2023-01-12 18:42:29 -0700

    feat: Allow shooting

---
+++ robot/src/main/java/org/frc1410/chargedup2023/subsystem/Shooter.java
@@ -0,0 +1,19 @@
+package org.frc1410.chargedup2023.subsystem;
+
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+public class Shooter implements Subsystem {
+    private final WPI_TalonFX motor = new WPI_TalonFX(0);
+
+    public Shooter() {
+        // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
+        //       in the constructor or in the robot coordination class, such as RobotContainer.
+    }
+
+    public void setSpeed(double speed) {
+        motor.set(speed);
+    }
+}
+
