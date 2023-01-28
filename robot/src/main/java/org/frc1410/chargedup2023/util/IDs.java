364536a86acc9d0c26e0ac10c124b9f40f141964
Mexican1oreo <Anthonyjeuc1234@gmail.com>
2023-01-27 19:42:01 -0700

    Feat: Lbork piston methods in subsystem

--- robot/src/main/java/org/frc1410/chargedup2023/util/IDs.java
+++ robot/src/main/java/org/frc1410/chargedup2023/util/IDs.java
@@ -6,11 +6,17 @@ public interface IDs {
     int DRIVETRAIN_LEFT_BACK_MOTOR_ID = 3;
     int DRIVETRAIN_RIGHT_BACK_MOTOR_ID = 4;
 
+    // Intake
     int INTAKE_MOTOR_ID = 5;
 
     int LBORK_OUTER_ROLLER_ID = 6;
     int LBORK_INNER_ROLLER_ID = 7;
 
+    // Intake pistons
     int INTAKE_FLIPPER_FOWARD_ID = 0;
     int INTAKE_FLIPPER_BACKWARD_ID = 1;
+
+    // Lebork piston
+    int LEBORK_PISTON_FOWARD_ID = 2;
+    int LEBORK_PISTON_BACKWARDS_ID = 3;
 }
