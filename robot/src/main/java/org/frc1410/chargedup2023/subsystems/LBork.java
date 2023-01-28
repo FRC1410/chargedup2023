e444b1ab7502e85664e714966a114c91e2ca6e29
Mexican1oreo <93799117+Mexican1oreo@users.noreply.github.com>
2023-01-27 20:44:53 -0700

    Feat: Lbork piston methods in subsystem (#32)
    * Feat: Lbork piston methods in subsystem
    
    * Feat: Lbork piston toggle method
    
    * Update robot/src/main/java/org/frc1410/chargedup2023/subsystems/LBork.java
    
    ---------
    
    Co-authored-by: Daedalus <16168171+RedDaedalus@users.noreply.github.com>

--- robot/src/main/java/org/frc1410/chargedup2023/subsystems/LBork.java
+++ robot/src/main/java/org/frc1410/chargedup2023/subsystems/LBork.java
@@ -3,14 +3,20 @@ package org.frc1410.chargedup2023.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 import static org.frc1410.chargedup2023.util.IDs.*;
 
 public class LBork implements Subsystem {
 
+    // lebork rollers
     private final CANSparkMax outerMotor = new CANSparkMax(LBORK_OUTER_ROLLER_ID, MotorType.kBrushless);
     private final CANSparkMax innerMotor = new CANSparkMax(LBORK_INNER_ROLLER_ID, MotorType.kBrushless);
+
+    //Lebork actuation
+    private final DoubleSolenoid lBorkpiston = new DoubleSolenoid(PneumaticsModuleType.REVPH, LEBORK_PISTON_FOWARD_ID, LEBORK_PISTON_BACKWARDS_ID);
 
     public LBork() {
         outerMotor.restoreFactoryDefaults();
@@ -20,6 +26,18 @@ public class LBork implements Subsystem {
     public void setRollerSpeeds(double innerRollerSpeed, double outerRollerSpeed) {
         innerMotor.set(innerRollerSpeed);
         outerMotor.set(outerRollerSpeed);
+    }
+
+    public void extend() {
+        lBorkpiston.set(DoubleSolenoid.Value.kForward);
+    }
+
+    public void retract() {
+        lBorkpiston.set(DoubleSolenoid.Value.kReverse);
+    }
+
+    public void toggle() {
+        lBorkpiston.toggle();
     }
 
 }
--- robot/src/main/java/org/frc1410/chargedup2023/util/IDs.java
+++ robot/src/main/java/org/frc1410/chargedup2023/util/IDs.java
@@ -1,16 +1,23 @@
 package org.frc1410.chargedup2023.util;
 
 public interface IDs {
+    // Drivetrain ID's
     int DRIVETRAIN_LEFT_FRONT_MOTOR_ID = 1;
     int DRIVETRAIN_RIGHT_FRONT_MOTOR_ID = 2;
     int DRIVETRAIN_LEFT_BACK_MOTOR_ID = 3;
     int DRIVETRAIN_RIGHT_BACK_MOTOR_ID = 4;
 
+    // Intake ID's
     int INTAKE_MOTOR_ID = 5;
 
     int LBORK_OUTER_ROLLER_ID = 6;
     int LBORK_INNER_ROLLER_ID = 7;
 
+    // Intake pistons ID's
     int INTAKE_FLIPPER_FOWARD_ID = 0;
     int INTAKE_FLIPPER_BACKWARD_ID = 1;
+
+    // Lebork piston ID's
+    int LEBORK_PISTON_FOWARD_ID = 2;
+    int LEBORK_PISTON_BACKWARDS_ID = 3;
 }
