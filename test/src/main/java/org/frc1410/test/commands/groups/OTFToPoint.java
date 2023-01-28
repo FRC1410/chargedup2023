0055f7cc01d2cb5f13f91c8cb503c76a326f75ca
Rowan <717538@dpsk12.net>
2023-01-28 16:58:43 -0700

    feat: Added souts for debugging

--- test/src/main/java/org/frc1410/test/commands/groups/OTFToPoint.java
+++ test/src/main/java/org/frc1410/test/commands/groups/OTFToPoint.java
@@ -3,6 +3,7 @@ package org.frc1410.test.commands.groups;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import org.frc1410.test.subsystems.Drivetrain;
@@ -33,7 +34,13 @@ public class OTFToPoint extends SequentialCommandGroup {
 		addRequirements(drivetrain);
 
 		addCommands(
-				command
+				command,
+				new InstantCommand(() -> {
+					System.out.println("Results");
+					System.out.println(Units.metersToInches(drivetrain.getPoseEstimation().getX()));
+					System.out.println(Units.metersToInches(drivetrain.getPoseEstimation().getY()));
+					System.out.println(drivetrain.getPoseEstimation().getRotation().getDegrees());
+				})
 		);
 	}
 }
