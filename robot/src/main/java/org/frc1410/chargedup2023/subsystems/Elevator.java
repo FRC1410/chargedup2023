88446fc72d5bdc9170bb9f8c6d7fbebee0c71ad4
Daedalus <16168171+RedDaedalus@users.noreply.github.com>
2023-01-28 09:42:42 -0700

    Update robot/src/main/java/org/frc1410/chargedup2023/subsystems/Elevator.java

--- robot/src/main/java/org/frc1410/chargedup2023/subsystems/Elevator.java
+++ robot/src/main/java/org/frc1410/chargedup2023/subsystems/Elevator.java
@@ -9,8 +9,8 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
 import static org.frc1410.chargedup2023.util.IDs.*;
 
 public class Elevator implements Subsystem {
-    private final CANSparkMax elevatorMotorOne = new CANSparkMax(ELEVATOR_MOTOR_ONE_ID, MotorType.kBrushed);
-    private final CANSparkMax elvatorMotorTwo = new CANSparkMax(ELEVATOR_MOTOR_TWO_ID, MotorType.kBrushed);
+    private final CANSparkMax leaderMotor = new CANSparkMax(ELEVATOR_MOTOR_ONE_ID, MotorType.kBrushed);
+    private final CANSparkMax followerMotor = new CANSparkMax(ELEVATOR_MOTOR_TWO_ID, MotorType.kBrushed);
 
     private final DigitalInput magSensor = new DigitalInput(0);
     
