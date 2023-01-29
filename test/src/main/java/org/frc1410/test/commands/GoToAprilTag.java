ea62d98ad6da1cf959f534797ec3692cafe244de
Rowan <717538@dpsk12.net>
2023-01-28 22:09:15 -0700

    feat: Removed field localization

--- test/src/main/java/org/frc1410/test/commands/GoToAprilTag.java
+++ test/src/main/java/org/frc1410/test/commands/GoToAprilTag.java
@@ -27,12 +27,14 @@ public class GoToAprilTag extends CommandBase {
 			if (RED_TAGS.contains(camera.getTarget().getFiducialId())) {
 				scheduler.scheduleAutoCommand(new OTFToPoint(
 						drivetrain,
-						new Pose2d(pose.getX() + Units.inchesToMeters(-40), FIELD_WIDTH - pose.getY(),
+//						new Pose2d(pose.getX() + Units.inchesToMeters(-40), FIELD_WIDTH - pose.getY(),
+//								pose.getRotation().toRotation2d().rotateBy(Rotation2d.fromDegrees(180)))));
+						new Pose2d(Units.inchesToMeters(-40), 0,
 								pose.getRotation().toRotation2d().rotateBy(Rotation2d.fromDegrees(180)))));
 			} else {
 				scheduler.scheduleAutoCommand(new OTFToPoint(
 						drivetrain,
-						new Pose2d(pose.getX() + Units.inchesToMeters(40), FIELD_WIDTH - pose.getY(),
+						new Pose2d(Units.inchesToMeters(40), 0,
 								pose.getRotation().toRotation2d())));
 			}
 		});
