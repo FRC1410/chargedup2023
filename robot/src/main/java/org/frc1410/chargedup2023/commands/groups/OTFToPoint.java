2f01cbfbbca96fb1864d33d87483a4fba0891066
Santiago Fiz <757966@dpsk12.net>
2023-02-04 10:29:53 -0700

    feat: OTFToPointCommand

---
+++ robot/src/main/java/org/frc1410/chargedup2023/commands/groups/OTFToPoint.java
@@ -0,0 +1,34 @@
+package org.frc1410.chargedup2023.commands.groups;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import org.frc1410.chargedup2023.subsystem.Drivetrain;
+import org.frc1410.chargedup2023.util.Trajectories;
+
+import java.util.List;
+
+import static org.frc1410.chargedup2023.util.Trajectories.*;
+
+public class OTFToPoint extends SequentialCommandGroup {
+    public OTFToPoint(Drivetrain drivetrain, Pose2d targetPose) {
+
+        RamseteCommand command = baseRamsete(
+                TrajectoryGenerator.generateTrajectory(List.of(drivetrain.getPoseEstimation(), targetPose),
+                        slowConfig), Trajectories.tunedFeedforward, leftControllerSlow, rightControllerSlow, drivetrain);
+
+        addRequirements(drivetrain);
+        addCommands(command);
+    }
+
+    public OTFToPoint(Drivetrain drivetrain, Translation2d midPose, Pose2d targetPose) {
+        RamseteCommand command = baseRamsete(
+                TrajectoryGenerator.generateTrajectory(drivetrain.getPoseEstimation(), List.of(midPose), targetPose,
+                        slowConfig), Trajectories.tunedFeedforward, leftControllerSlow, rightControllerSlow, drivetrain);
+
+        addRequirements(drivetrain);
+        addCommands(command);
+    }
+}
