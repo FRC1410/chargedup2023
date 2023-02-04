1e1771c824a8ef5af38b1802b1eb822fbbac8db2
Daedalus <16168171+RedDaedalus@users.noreply.github.com>
2023-02-04 12:48:51 -0700

    feat: LEDs ee (#41)
    * feat: LEDs ee
    
    * make it universal

---
+++ robot/src/main/java/org/frc1410/chargedup2023/subsystems/LightBar.java
@@ -0,0 +1,25 @@
+package org.frc1410.chargedup2023.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import org.frc1410.chargedup2023.util.IDs;
+
+public class LightBar implements Subsystem {
+
+	private final PWMSparkMax blinkin = new PWMSparkMax(IDs.BLINKIN_PORT);
+
+	public void set(Profile profile) {
+		blinkin.set(profile.id);
+	}
+
+	public enum Profile {
+		;
+
+		private final double id;
+
+		// Find available IDs at https://www.revrobotics.com/content/docs/REV-11-1105-UM.pdf
+		Profile(double id) {
+			this.id = id;
+		}
+	}
+}
