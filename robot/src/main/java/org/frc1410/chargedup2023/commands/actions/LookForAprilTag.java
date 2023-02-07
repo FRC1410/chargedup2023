9b82bd013f270cca9739ec6a08db6aa18eee93a9
Rowan Eklund <717538@dpsk12.net>
2023-02-06 18:31:41 -0700

    breaking: Extrapolation

--- robot/src/main/java/org/frc1410/chargedup2023/commands/actions/LookForAprilTag.java
+++ robot/src/main/java/org/frc1410/chargedup2023/commands/actions/LookForAprilTag.java
@@ -3,8 +3,10 @@ package org.frc1410.chargedup2023.commands.actions;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import org.frc1410.chargedup2023.commands.actions.drivetrain.GoToAprilTag;
+import org.frc1410.chargedup2023.commands.groups.teleop.SubstationScoringMode;
 import org.frc1410.chargedup2023.subsystems.Drivetrain;
 import org.frc1410.chargedup2023.subsystems.ExternalCamera;
+import org.frc1410.chargedup2023.subsystems.LBork;
 import org.frc1410.framework.control.Button;
 import org.frc1410.framework.control.observer.WhileHeldObserver;
 import org.frc1410.framework.scheduler.task.TaskPersistence;
@@ -20,13 +22,15 @@ public class LookForAprilTag extends CommandBase {
 	private final Button button;
 	private final Drivetrain drivetrain;
 	private final ExternalCamera camera;
+	private final LBork lbork;
 	private final TaskScheduler scheduler;
 	private final boolean rightBumper;
 
-	public LookForAprilTag(Button button, Drivetrain drivetrain, ExternalCamera camera, TaskScheduler scheduler, boolean rightBumper) {
+	public LookForAprilTag(Button button, Drivetrain drivetrain, ExternalCamera camera, LBork lbork, TaskScheduler scheduler, boolean rightBumper) {
 		this.button = button;
 		this.drivetrain = drivetrain;
 		this.camera = camera;
+		this.lbork = lbork;
 		this.scheduler = scheduler;
 		this.rightBumper = rightBumper;
 	}
@@ -39,13 +43,12 @@ public class LookForAprilTag extends CommandBase {
 			// TODO: Replace GoToAprilTag command with the setup command
 			if (camera.getTarget().getFiducialId() == 4 || camera.getTarget().getFiducialId() == 5) {
 				scheduler.schedule(
-					new CommandTask(new GoToAprilTag(
+					new CommandTask(new SubstationScoringMode(
 						drivetrain,
 						camera,
+						lbork,
+						scheduler,
 						rightBumper
-							? GoToAprilTag.Node.RIGHT_SUBSTATION
-							: GoToAprilTag.Node.LEFT_SUBSTATION,
-						scheduler
 					)),
 					TaskPersistence.EPHEMERAL,
 					observer,
