2a97cecb3b3b5f230b48eb750c887b3683565538
Rowan Eklund <717538@dpsk12.net>
2023-02-17 08:39:09 -0700

    breaking(POIs): Added internal waypoint functionality when you are ready Santi

--- robot/src/main/java/org/frc1410/chargedup2023/commands/actions/drivetrain/GoToAprilTag.java
+++ robot/src/main/java/org/frc1410/chargedup2023/commands/actions/drivetrain/GoToAprilTag.java
@@ -40,7 +40,7 @@ public class GoToAprilTag extends CommandBase {
 				switch (targetNode) {
 					case LEFT_YANKEE_NODE -> {
 						if (camera.getTarget().getFiducialId() == 1 && waypointFlag)
-							scheduler.scheduleAutoCommand(new OTFToPoint(drivetrain, pose.toPose2d(), RED_OUTSIDE_WAYPOINT, RED_LEFT_YANKEE_NODE));
+							scheduler.scheduleAutoCommand(new OTFToPoint(drivetrain, pose.toPose2d(), RED_OUTSIDE_WAYPOINT, RED_LEFT_YANKEE_ANGLED_NODE));
 						else
 							scheduler.scheduleAutoCommand(new OTFToPoint(drivetrain, pose.toPose2d(), RED_LEFT_YANKEE_NODE));
 					}
@@ -48,7 +48,7 @@ public class GoToAprilTag extends CommandBase {
 							scheduler.scheduleAutoCommand(new OTFToPoint(drivetrain, pose.toPose2d(), RED_PAPA_NODE));
 					case RIGHT_YANKEE_NODE -> {
 						if (camera.getTarget().getFiducialId() == 3 && waypointFlag)
-							scheduler.scheduleAutoCommand(new OTFToPoint(drivetrain, pose.toPose2d(), RED_BARRIER_WAYPOINT, RED_RIGHT_YANKEE_NODE));
+							scheduler.scheduleAutoCommand(new OTFToPoint(drivetrain, pose.toPose2d(), RED_BARRIER_WAYPOINT, RED_RIGHT_YANKEE_ANGLED_NODE));
 						else
 							scheduler.scheduleAutoCommand(new OTFToPoint(drivetrain, pose.toPose2d(), RED_RIGHT_YANKEE_NODE));
 					}
@@ -65,7 +65,7 @@ public class GoToAprilTag extends CommandBase {
 				switch (targetNode) {
 					case LEFT_YANKEE_NODE -> {
 						if (camera.getTarget().getFiducialId() == 6)
-							scheduler.scheduleAutoCommand(new OTFToPoint(drivetrain, pose.toPose2d(), BLUE_BARRIER_WAYPOINT, BLUE_LEFT_YANKEE_NODE));
+							scheduler.scheduleAutoCommand(new OTFToPoint(drivetrain, pose.toPose2d(), BLUE_BARRIER_WAYPOINT, BLUE_LEFT_YANKEE_ANGLED_NODE));
 						else
 							scheduler.scheduleAutoCommand(new OTFToPoint(drivetrain, pose.toPose2d(), BLUE_LEFT_YANKEE_NODE));
 					}
@@ -73,7 +73,7 @@ public class GoToAprilTag extends CommandBase {
 							scheduler.scheduleAutoCommand(new OTFToPoint(drivetrain, pose.toPose2d(), BLUE_PAPA_NODE));
 					case RIGHT_YANKEE_NODE -> {
 						if (camera.getTarget().getFiducialId() == 8)
-							scheduler.scheduleAutoCommand(new OTFToPoint(drivetrain, pose.toPose2d(), BLUE_OUTSIDE_WAYPOINT, BLUE_RIGHT_YANKEE_NODE));
+							scheduler.scheduleAutoCommand(new OTFToPoint(drivetrain, pose.toPose2d(), BLUE_OUTSIDE_WAYPOINT, BLUE_RIGHT_YANKEE_ANGLED_NODE));
 						else
 							scheduler.scheduleAutoCommand(new OTFToPoint(drivetrain, pose.toPose2d(), BLUE_RIGHT_YANKEE_NODE));
 					}
