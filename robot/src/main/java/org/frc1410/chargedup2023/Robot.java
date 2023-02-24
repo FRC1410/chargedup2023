258698346620d7d91d5fd1f3f675dd61130a3cca
Santiago Fiz <757966@dpsk12.net>
2023-02-23 22:15:55 -0700

    feat: LBork papa intake mode runs with papa intake

--- robot/src/main/java/org/frc1410/chargedup2023/Robot.java
+++ robot/src/main/java/org/frc1410/chargedup2023/Robot.java
@@ -81,6 +81,7 @@ public final class Robot extends PhaseDrivenRobot {
 		scheduler.scheduleDefaultCommand(
 				new RunIntakeLooped(
 						intake,
+						lBork,
 						operatorController.LEFT_TRIGGER,
 						operatorController.RIGHT_TRIGGER),
 				TaskPersistence.GAMEPLAY
