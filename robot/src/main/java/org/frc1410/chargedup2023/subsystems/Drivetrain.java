ffae28634cafcf7943138fb1166aaf431a7d2bda
Daedalus <16168171+RedDaedalus@users.noreply.github.com>
2023-03-01 17:43:25 -0700

    chore: add patched NavX client (to the right branch this time)

--- robot/src/main/java/org/frc1410/chargedup2023/subsystems/Drivetrain.java
+++ robot/src/main/java/org/frc1410/chargedup2023/subsystems/Drivetrain.java
@@ -1,6 +1,6 @@
 package org.frc1410.chargedup2023.subsystems;
 
-import com.kauailabs.navx.frc.AHRS;
+import com.kauailabs.navx2.frc.AHRS;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
