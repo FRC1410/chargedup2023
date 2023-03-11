87d8db0711412a8fead8abece8ae7c1ea02dc10a
Rowan Eklund <717538@dpsk12.net>
2023-03-11 09:07:19 -0600

    fix(Auto): Better waypoint

--- robot/src/main/java/org/frc1410/chargedup2023/auto/POIs.java
+++ robot/src/main/java/org/frc1410/chargedup2023/auto/POIs.java
@@ -56,13 +56,11 @@ public interface POIs {
 	Translation2d BARRIER_CUBE_SCORE_MIDPOINT = new Translation2d(inchesToMeters(215), inchesToMeters(177.19));
 	Translation2d OUTSIDE_CUBE_SCORE_MIDPOINT = new Translation2d(inchesToMeters(215), inchesToMeters(38.31));
 
-
-
 	// Auto
 	// Blue
-	Pose2d BLUE_BARRIER_GRID = new Pose2d(inchesToMeters(74.25), inchesToMeters(196.19), Rotation2d.fromDegrees(180));
+	Pose2d BLUE_BARRIER_GRID = new Pose2d(inchesToMeters(74.25), inchesToMeters(192.19), Rotation2d.fromDegrees(180));
 	Pose2d BLUE_OKLAHOMA = new Pose2d(inchesToMeters(293), inchesToMeters(235.5), Rotation2d.fromDegrees(-115));
-	Translation2d BLUE_OKLAHOMA_MIDPOINT = new Translation2d(inchesToMeters(148.5), inchesToMeters(196.19));
+	Translation2d BLUE_OKLAHOMA_MIDPOINT = new Translation2d(inchesToMeters(148.5), inchesToMeters(192.19));
 	Translation2d BLUE_OKLAHOMA_MIDPOINT2 = new Translation2d(inchesToMeters(239.4), inchesToMeters(202));
 	Translation2d BLUE_OKLAHOMA_G302 = new Translation2d(inchesToMeters(279.5), inchesToMeters(185));
 	Translation2d BLUE_OKLAHOMA_BACK = new Translation2d(inchesToMeters(170), inchesToMeters(190));
@@ -71,9 +69,9 @@ public interface POIs {
 	Pose2d BLUE_EXTERNAL_ENGAGE = new Pose2d(inchesToMeters(217), inchesToMeters(133.5), Rotation2d.fromDegrees(180));
 
 	// Red
-	Pose2d RED_BARRIER_GRID = new Pose2d(inchesToMeters(576.97), inchesToMeters(196.19), Rotation2d.fromDegrees(0));
+	Pose2d RED_BARRIER_GRID = new Pose2d(inchesToMeters(576.97), inchesToMeters(192.19), Rotation2d.fromDegrees(0));
 	Pose2d RED_OKLAHOMA = new Pose2d(inchesToMeters(358.22), inchesToMeters(235.5), Rotation2d.fromDegrees(-65));
-	Translation2d RED_OKLAHOMA_MIDPOINT = new Translation2d(inchesToMeters(502.72), inchesToMeters(196.19));
+	Translation2d RED_OKLAHOMA_MIDPOINT = new Translation2d(inchesToMeters(502.72), inchesToMeters(192.19));
 	Translation2d RED_OKLAHOMA_MIDPOINT2 = new Translation2d(inchesToMeters(411.82), inchesToMeters(202));
 	Translation2d RED_OKLAHOMA_G302 = new Translation2d(inchesToMeters(371.72), inchesToMeters(185));
 	Translation2d RED_OKLAHOMA_BACK = new Translation2d(inchesToMeters(481.22), inchesToMeters(190));
