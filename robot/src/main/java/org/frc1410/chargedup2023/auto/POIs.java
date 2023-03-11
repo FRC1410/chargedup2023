430841b3c52c92bc499076b71bf7875d3c1c29a4
Rowan Eklund <717538@dpsk12.net>
2023-03-10 22:08:07 -0600

    fix: Resimmed 2 game piece, will probably work now

--- robot/src/main/java/org/frc1410/chargedup2023/auto/POIs.java
+++ robot/src/main/java/org/frc1410/chargedup2023/auto/POIs.java
@@ -16,9 +16,9 @@ public interface POIs {
 	Translation2d BLUE_OKLAHOMA_MIDPOINT = new Translation2d(inchesToMeters(148.5), inchesToMeters(196.19));
 	Translation2d BLUE_OKLAHOMA_MIDPOINT2 = new Translation2d(inchesToMeters(239.4), inchesToMeters(202));
 	Translation2d BLUE_OKLAHOMA_G302 = new Translation2d(inchesToMeters(279.5), inchesToMeters(185));
-	Translation2d BLUE_OKLAHOMA_BACK = new Translation2d(inchesToMeters(190), inchesToMeters(185));
+	Translation2d BLUE_OKLAHOMA_BACK = new Translation2d(inchesToMeters(170), inchesToMeters(190));
 	Translation2d BLUE_OKLAHOMA_GAMEPIECE = new Translation2d(inchesToMeters(278.25), inchesToMeters(180.2));
-	Pose2d BLUE_BARRIER_SCORE_PAPA = new Pose2d(inchesToMeters(68.25), inchesToMeters(176.19), Rotation2d.fromDegrees(180));
+	Pose2d BLUE_BARRIER_SCORE_PAPA = new Pose2d(inchesToMeters(81), inchesToMeters(176.1), Rotation2d.fromDegrees(180));
 	Pose2d BLUE_EXTERNAL_ENGAGE = new Pose2d(inchesToMeters(217), inchesToMeters(133.5), Rotation2d.fromDegrees(180));
 
 	// Red
@@ -27,25 +27,9 @@ public interface POIs {
 	Translation2d RED_OKLAHOMA_MIDPOINT = new Translation2d(inchesToMeters(502.72), inchesToMeters(196.19));
 	Translation2d RED_OKLAHOMA_MIDPOINT2 = new Translation2d(inchesToMeters(411.82), inchesToMeters(202));
 	Translation2d RED_OKLAHOMA_G302 = new Translation2d(inchesToMeters(371.72), inchesToMeters(185));
-	Translation2d RED_OKLAHOMA_BACK = new Translation2d(inchesToMeters(461.22), inchesToMeters(185));
+	Translation2d RED_OKLAHOMA_BACK = new Translation2d(inchesToMeters(481.22), inchesToMeters(190));
 	Translation2d RED_OKLAHOMA_GAMEPIECE = new Translation2d(inchesToMeters(372.97), inchesToMeters(180.2));
-	Pose2d RED_BARRIER_SCORE_PAPA = new Pose2d(inchesToMeters(582.97), inchesToMeters(176.19), Rotation2d.fromDegrees(0));
+	Pose2d RED_BARRIER_SCORE_PAPA = new Pose2d(inchesToMeters(570.22), inchesToMeters(176.1), Rotation2d.fromDegrees(0));
 	Pose2d RED_EXTERNAL_ENGAGE = new Pose2d(inchesToMeters(434.22), inchesToMeters(133.5), Rotation2d.fromDegrees(0));
 
-	// AprilTag Alignment
-	Pose2d RED_LEFT_YANKEE_NODE = new Pose2d(inchesToMeters(40.5), -inchesToMeters(22), new Rotation2d());
-	Pose2d RED_PAPA_NODE = new Pose2d(inchesToMeters(39.5), 0, new Rotation2d());
-	Pose2d RED_RIGHT_YANKEE_NODE = new Pose2d(inchesToMeters(40.5), inchesToMeters(22), new Rotation2d());
-	Translation2d RED_BARRIER_WAYPOINT = new Translation2d(inchesToMeters(525), FIELD_WIDTH - inchesToMeters(132)); //TODO
-	Translation2d RED_OUTSIDE_WAYPOINT = new Translation2d(inchesToMeters(525), inchesToMeters(34));
-	Pose2d RED_LEFT_SUBSTATION = new Pose2d(inchesToMeters(52), -inchesToMeters(33), Rotation2d.fromDegrees(180));
-	Pose2d RED_RIGHT_SUBSTATION = new Pose2d(inchesToMeters(52), inchesToMeters(33), Rotation2d.fromDegrees(180));
-
-	Pose2d BLUE_LEFT_YANKEE_NODE = new Pose2d(inchesToMeters(40.5), -inchesToMeters(22), Rotation2d.fromDegrees(180));
-	Pose2d BLUE_PAPA_NODE = new Pose2d(inchesToMeters(39.5), 0, Rotation2d.fromDegrees(180));
-	Pose2d BLUE_RIGHT_YANKEE_NODE = new Pose2d(inchesToMeters(40.5), inchesToMeters(22), Rotation2d.fromDegrees(180));
-	Translation2d BLUE_BARRIER_WAYPOINT = new Translation2d(inchesToMeters(115), FIELD_WIDTH - inchesToMeters(132)); //TODO
-	Translation2d BLUE_OUTSIDE_WAYPOINT = new Translation2d(inchesToMeters(115), FIELD_WIDTH - inchesToMeters(284.5)); //TODO
-	Pose2d BLUE_LEFT_SUBSTATION = new Pose2d(inchesToMeters(52), -inchesToMeters(33), new Rotation2d());
-	Pose2d BLUE_RIGHT_SUBSTATION = new Pose2d(inchesToMeters(52), inchesToMeters(33), new Rotation2d());
 }
